a4a171702004797e309fbe8a93e8688f65e68618
CommanderRedYT <ccommanderred@gmail.com>
2024-12-11 16:44:31 +0100

    Implement product search

--- src/api/products/index.ts
+++ src/api/products/index.ts
@@ -1,5 +1,6 @@
 import type { ExpressResponse } from 'api';
-import { listProducts } from 'database/products';
+import type { SearchedProducts } from 'database/products';
+import { listProducts, searchProducts } from 'database/products';
 import express from 'express';
 import type Products from 'schemas/public/Products';
 
@@ -8,6 +9,51 @@ const productsRouter = express.Router();
 productsRouter.get('/list', async (_req, res: ExpressResponse<Products[]>) => {
     const products = await listProducts();
     res.json({ success: true, data: products });
+});
+
+productsRouter.get(
+    '/search',
+    async (req, res: ExpressResponse<SearchedProducts[]>) => {
+        const searchQuery = req.query.query;
+
+        if (!searchQuery) {
+            res.status(400).json({
+                success: false,
+                error: 'No search query provided',
+            });
+            return;
+        }
+
+        const products = await searchProducts(searchQuery.toString());
+
+        res.json({ success: true, data: products });
+    },
+);
+
+productsRouter.get('/get', async (req, res: ExpressResponse<Products>) => {
+    const { id } = req.query;
+
+    if (!id) {
+        res.status(400).json({
+            success: false,
+            error: 'No ID provided',
+        });
+        return;
+    }
+
+    const products = await listProducts();
+
+    const product = products.find(p => p.id === parseInt(id as string, 10));
+
+    if (!product) {
+        res.status(404).json({
+            success: false,
+            error: 'Product not found',
+        });
+        return;
+    }
+
+    res.json({ success: true, data: product });
 });
 
 export default productsRouter;
--- src/database/products/index.ts
+++ src/database/products/index.ts
@@ -1,4 +1,6 @@
 import getClient from 'database/db';
+// @ts-expect-error: Typescript is deppat.
+import Fuse from 'fuse.js';
 import type Products from 'schemas/public/Products';
 
 export const listProducts = async (): Promise<Products[]> => {
@@ -11,6 +13,45 @@ export const listProducts = async (): Promise<Products[]> => {
             ...row,
             price_per_unit: parseFloat(row.price_per_unit),
         }));
+    } finally {
+        client.release();
+    }
+};
+
+export interface SearchedProducts {
+    product: Products;
+    score: number;
+}
+
+export const searchProducts = async (
+    query: string,
+): Promise<SearchedProducts[]> => {
+    const client = await getClient();
+
+    try {
+        const { rows } = await client.query('SELECT * FROM products');
+
+        const fuseInstance = new Fuse<Products>(rows, {
+            keys: ['name', 'description'],
+            includeScore: true,
+            threshold: 0.35,
+            shouldSort: true,
+        });
+
+        const results = fuseInstance.search(query);
+
+        return results.map(
+            result =>
+                ({
+                    product: {
+                        ...result.item,
+                        price_per_unit: parseFloat(
+                            result.item.price_per_unit as unknown as string,
+                        ),
+                    },
+                    score: result.score,
+                }) as SearchedProducts,
+        );
     } finally {
         client.release();
     }
